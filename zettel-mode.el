9feadc2ee33b02ca4f6327c23349dce4cbd3ac3c
felko <baxbaxwalanuksiwe@gmail.com>
2020-04-04 12:53:12 +0200

    Implemented follow for z(cf)query links

--- zettel-mode.el
+++ zettel-mode.el
@@ -21,6 +21,7 @@
 (require 'ivy)
 (require 'subr-x)
 (require 'counsel)
+(require 'json)
 
 (defconst neuron-dir (getenv "ZETTELPATH"))
 
@@ -36,12 +37,22 @@
   "Return the path of the zettelkasten."
   (f-join "/" neuron-dir neuron-zettelkasten-name))
 
-(defun neuron-command (cmd)
+(defun neuron--command (cmd)
   "Run a neuron command in the current zettekasten.
 CMD should be a string representing the command"
   (shell-command-to-string (concat "neuron " (neuron-zettelkasten-path) " " cmd)))
 
-(defun neuron-rib-command (cmd)
+(defun neuron--json-extract-info (match)
+  "Extract Zettel ID and title from MATCH, as a JSON string."
+  (let ((lst (json-read-from-string match)))
+    (mapcar (lambda (obj) (list (map-elt obj 'id) (map-elt obj 'title))) lst)))
+
+(defun neuron--query-url-command (query-url)
+  "Run a neuron query from a zquery QUERY-URL."
+  (let* ((res (shell-command-to-string (concat "neuron " (neuron-zettelkasten-path) " query --uri '" query-url "'"))))
+    (neuron--json-extract-info res)))
+
+(defun neuron--rib-command (cmd)
   "Run a neuron command to manage the web application.
 CMD is a string representing a neuron rib command."
   (start-process "neuron-rib" "*neuron-rib*" "neuron" (neuron-zettelkasten-path) "rib" cmd))
@@ -58,7 +69,7 @@ CMD is a string representing a neuron rib command."
 (defun neuron-new-zettel ()
   "Create a new zettel."
   (interactive)
-  (let* ((zettel-path   (string-trim-right (neuron-command "new \"\"")))
+  (let* ((zettel-path   (string-trim-right (neuron--command "new \"\"")))
          (zettel-buffer (find-file-noselect zettel-path)))
     (and
      (pop-to-buffer-same-window zettel-buffer)
@@ -72,6 +83,15 @@ CMD is a string representing a neuron rib command."
   (interactive)
   (let ((match (counsel-rg "title: " (neuron-zettelkasten-path) "--no-line-number --no-heading --sort path" "Select Zettel: ")))
     (f-base (car (split-string match ":")))))
+
+(defun neuron--select-zettel-from-query (query-url)
+  "Select a zettel from the match of QUERY-URL."
+  (ivy-read "Select Zettel: "
+            (mapcar (lambda (z) (propertize (format "[%s] %s" (nth 0 z) (nth 1 z)) 'id (nth 0 z)))
+             (neuron--query-url-command query-url))
+            ; :predicate  (lambda (path) (not (string-prefix-p "." path)))
+            :action (lambda (z) (neuron--edit-zettel-from-id (get-text-property 0 'id z)))
+            :caller 'neuron-select-zettel-from-query))
 
 (defun neuron-edit-zettel ()
   "Edit a zettel."
@@ -92,7 +112,7 @@ CMD is a string representing a neuron rib command."
 (defun neuron-insert-new-zettel ()
   "Create a new zettel."
   (interactive)
-  (let* ((path   (string-trim-right (neuron-command "new \"\"")))
+  (let* ((path   (string-trim-right (neuron--command "new \"\"")))
          (id     (f-base (f-no-ext path)))
          (buffer (find-file-noselect path)))
     (and
@@ -103,7 +123,7 @@ CMD is a string representing a neuron rib command."
      (end-of-line)
      (message (concat "Created " path)))))
 
-(defun neuron-edit-zettel-from-path (path &optional before after)
+(defun neuron--edit-zettel-from-path (path &optional before after)
   "Open a neuron zettel from PATH.
 Execute BEFORE just before popping the buffer and AFTER just after enabling `zettel-mode'."
   (let* ((buffer (find-file-noselect path)))
@@ -113,27 +133,27 @@ Execute BEFORE just before popping the buffer and AFTER just after enabling `zet
      (zettel-mode)
      (if after (funcall after) t))))
 
-(defun neuron-edit-zettel-from-id (id &optional before after)
+(defun neuron--edit-zettel-from-id (id &optional before after)
   "Open a neuron zettel from ID.
 Execute BEFORE just before popping the buffer and AFTER just after enabling `zettel-mode'."
-  (neuron-edit-zettel-from-path
+  (neuron--edit-zettel-from-path
    (f-join "/" (neuron-zettelkasten-path) (format "%s.md" id))
    before
    after))
 
 (defun neuron-follow-thing-at-point ()
-  "Open the zettel link at POS."
+  "Open the zettel link at point."
   (interactive)
   (let* ((link   (markdown-link-at-pos (point)))
          (id     (nth 2 link))
          (url    (nth 3 link))
          (struct (url-generic-parse-url url))
          (type   (url-type struct)))
-    (if (string-equal type "z")
-        (neuron-edit-zettel-from-id id)
-      (markdown-follow-thing-at-point (point)))
-    (message "%S" (list type id)))
-  )
+    (pcase type
+      ("z"        (neuron--edit-zettel-from-id id))
+      ("zquery"   (neuron--select-zettel-from-query url))
+      ("zcfquery" (neuron--select-zettel-from-query url))
+      (_          (markdown-follow-thing-at-point link)))))
 
 (defun neuron-rib-serve ()
   "Start a web app for browsing the zettelkasten."
@@ -141,7 +161,7 @@ Execute BEFORE just before popping the buffer and AFTER just after enabling `zet
   (if neuron-rib-has-running-instance
       (message "A rib instance is already running")
     (and
-     (neuron-rib-command "serve")
+     (neuron--rib-command "serve")
      (setq neuron-rib-has-running-instance t)
      (message "Started web application on localhost:8080"))))
 
