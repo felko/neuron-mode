3089b88f43e831255f856289f401e8abdc154c86
Sridhar Ratnakumar <srid@srid.ca>
2020-04-14 19:43:27 -0400

    Add neuron-rib-generate
    With multiple zettelkastens, I rarely use `watch`. So doing an one-off
    generation right after editing a few zettels is a typical use case for
    me.

--- zettel-mode.el
+++ zettel-mode.el
@@ -65,6 +65,10 @@ returned as a string."
   "Run an asynchronous neuron process spawned by the rib command with arguments ARGS."
   (start-process-shell-command "rib" "*rib*" (apply #'neuron--make-command "rib" args)))
 
+(defun neuron--run-rib-compile (&rest args)
+  "Run an synchronous neuron command spawned by the rib command with arguments ARGS."
+  (compile (apply #'neuron--make-command "rib" args)))
+
 (defun neuron-select-zettelkasten ()
   "Select the active zettelkasten."
   (interactive)
@@ -195,6 +199,13 @@ Execute BEFORE just before popping the buffer and AFTER just after enabling `zet
   (interactive)
   (if (neuron--run-rib-process "-wS")
       (message "Started web application on localhost:8080")
+    (message "Rib command failed")))
+
+(defun neuron-rib-generate ()
+  "Do an one-off generation of the web interface of the zettelkasten."
+  (interactive)
+  (if (neuron--run-rib-compile)
+      (message "Generated HTML files")
     (message "Rib command failed")))
 
 (defun neuron-rib-open-page (page)
